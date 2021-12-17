d64cad1f9adf7031cdf934b8139ac0f075bbec53
Angeles <angi.zerep@gmail.com>
2021-12-16 22:47:09 -0400

    modif

--- reto#15/reto15.js
+++ reto#15/reto15.js
@@ -1,5 +1,24 @@
 // export default function checkSledJump(heights) {
 function checkSledJump(heights) {
     // ¡No olvides compartir tu solución en redes!
+
+    let sube = true
+    let baja = true
+
+    for ( let i = 0 ; i < heights.length ; i++ ){
+        console.log('aca ')
+
+        console.log('heights[i] ',heights[i])
+
+        if ( heights[i] === ( heights[i + 1] - 1 ) ){
+            console.log('sube ')
+        }
+    }
+
+    console.log('aca ', heights)
     return false
 }
+
+const sledJump = checkSledJump([1, 2, 3, 2, 1]) // true: sube y baja de forma estricta
+
+console.log(sledJump)
