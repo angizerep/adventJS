dc8f6cd6f733dca9e286822a42593bc87a766951
Angeles <angi.zerep@gmail.com>
2021-12-24 22:52:53 -0400

    solucion reto 18

--- reto#18/reto18.js
+++ reto#18/reto18.js
@@ -1,35 +1,33 @@
 // export default function fixFiles(files) {
 function fixFiles(files) {
 
-    console.log(' ACA ADENTRO ',files)
-
     let arrayFiltered = removeDuplicates(files)
     const original = files
-
-    console.log('arrayFiltered ', arrayFiltered)
 
     for ( let i = 0 ; i < arrayFiltered.length ; i ++ ){
 
         let k = 0
-
-        console.log('iiiiiii  ', arrayFiltered[i])
-
         for ( let m = 0 ; m < original.length ; m ++  ){
 
-            if ( arrayFiltered[i] === original[m] && k === 0 ){
-                k++
-                console.log('ES EL PRIMER ELEMENTO')
-            } else if( arrayFiltered[i] === original[m] && k > 0 ){
-                k++
-                console.log('TIENE MAS DE 1 ELEMENTO y K es ', k)
-                files[m] = original[m] + '('+k+')'
-            }else{
-                console.log('PS NO HACE NADA')
+            if ( original[m].includes('(')  ){
+                if ( arrayFiltered[i] === original[m] && k === 0 ){
+                    k++
+                } else if( arrayFiltered[i] === original[m] && k > 0 ){
+                    files[m] = original[m] + '('+k+')'
+                    k++
+                }
+            }
+            else {
+                if ( arrayFiltered[i] === original[m] && k === 0 ){
+                    k++
+                } else if( arrayFiltered[i] === original[m] && k > 0 ){
+                    files[m] = original[m] + '('+k+')'
+                    k++
+
+                }
             }
         }
     }
-
-    // ¡No olvides compartir tu solución en redes!
     return files
 }
 
@@ -47,31 +45,8 @@ function removeDuplicates(arr) {
 
 
 // // ojo que los elfos ya tenían archivos con (1)... ¡y pueden estar repetidos!
-const files = ['file', 'file(1)', 'icon', 'icon(1)', 'icon(1)']
-fixFiles(files) // ['file', 'file(1)', 'icon', 'icon(1)', 'icon(1)(1)']
+// const files = ['file', 'file(1)', 'icon', 'icon(1)', 'icon(1)']
+// fixFiles(files) // ['file', 'file(1)', 'icon', 'icon(1)', 'icon(1)(1)']
 
 
 console.log( files )
-
-
-/*
-let myArray = [
-    {id: 0, name: "Jhon"},
-    {id: 1, name: "Sara"},
-    {id: 2, name: "Domnic"},
-    {id: 3, name: "Bravo"}
-],
-
-    //Find index of specific object using findIndex method.    
-    objIndex = myArray.findIndex((obj => obj.id == 1));
-
-    //Log object to Console.
-    console.log("Before update: ", myArray[objIndex])
-
-    //Update object's name property.
-    myArray[objIndex].name = "Laila"
-
-    //Log object to console again.
-    console.log("After update: ", myArray[objIndex])
-
-*/
