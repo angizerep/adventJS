9103893d62f1a0205b834a4e9925c0e8b888c1bc
Angeles <angi.zerep@gmail.com>
2021-12-25 18:33:29 -0400

    modif

---
+++ reto#25/reto25.js
@@ -0,0 +1,8 @@
+// export default function canMouseEat(direction, game) {
+function canMouseEat(direction, game) {
+
+    // ¡Gracias por jugar a AdventJS 2021! 🤗
+    // ¡Nos vemos el año que viene! 👋
+    // Por favor, comparte en las redes qué te ha parecido! 🚀
+    return false
+}
